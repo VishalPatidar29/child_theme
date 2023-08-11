98061badc7c2074756c3ab8bb77204fa923c7d59
Vishal <er.vishalpatidar29@gmail.com>
2023-08-11 16:08:07 +0530

    This is first commit add the function, style , screenshot files

---
+++ function.php
@@ -0,0 +1,13 @@
+<?php
+ 
+ 
+add_action( 'wp_enqueue_scripts', 'hubspot_blog_theme_enqueue_styles' );
+ 
+ 
+function hubspot_blog_theme_enqueue_styles() {
+ 
+ 
+wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+ 
+ 
+}
